694a71ae53c1d1e6d1f121ccb2e5dde0a9e73f81
north-tower <muneshmyke@gmail.com>
2023-04-09 09:03:01 +0300

    App Initialized

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ export default function Banner() {
         <Image 
         layout="fill"
         style={{ objctFit:"cover"}}
-        src="https://links.papareact.com/0fm" />
+        src="https://links.papareact.com/0fm" alt='Car' />
         <div className='absolute top-1/2 w-full text-center'>
             <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
             <button className='text-purple-500 bg-white px-10 py-4 shadow-md
