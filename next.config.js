55af997b342b096a4d8c79a530d2d1d1f73b9de2
north-tower <muneshmyke@gmail.com>
2023-04-13 15:47:20 +0300

    'search'

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
 
 module.exports = {
   images :{
-    domains:["links.papareact.com"]
+    domains:["links.papareact.com",
+    "a0.muscache.com"]
 },
+env: {
+  mapbox_key:'pk.eyJ1IjoibWlraTAwNyIsImEiOiJjbGNxNHd2aGkwMmg1M29reWd2ZGJod2M1In0.f9-OPY7z8IFoBGwdM7zUZw'
+}
 
 } ,{nextConfig}
